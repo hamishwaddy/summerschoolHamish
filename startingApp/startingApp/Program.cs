df56f7aba811b19aed57f346e5b11114928e418f
Hamish Wadsworth <hamishwaddy@gmail.com>
2018-12-12 19:52:37 +1300

    changed firstApp

--- startingApp/startingApp/Program.cs
+++ startingApp/startingApp/Program.cs
@@ -12,7 +12,7 @@ namespace startingApp
             {
                 Console.WriteLine("Type a 2-digit number");
                 num1 = int.Parse(Console.ReadLine());
-            } while (num1 <= 10 || num1 >= 100);
+            } while (num1 <= 9 || num1 >= 100);
 
 
             int num2 = 0;
@@ -20,10 +20,12 @@ namespace startingApp
             {
                 Console.WriteLine("Next, type a 3-digit number");
                 num2 = int.Parse(Console.ReadLine());
-            } while (num2 <= 100 || num2 >= 1000);
+            } while (num2 <= 99 || num2 >= 1000);
 
             Console.WriteLine("FULL EQUATION");
             Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{1} / {0} = {2}", num2, num1, num2 / num1);
         }
 
     }
