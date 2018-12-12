7032d80d4b98fd042e6339daf141e9cfdd8ca07c
Hamish Wadsworth <hamishwaddy@gmail.com>
2018-12-12 20:55:16 +1300

    added firstLastName project

---
+++ firstSecondName/firstSecondName/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace firstSecondName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Now type your last name: ");
+            string lastName = Console.ReadLine();
+            Display(firstName, lastName);
+
+        }
+
+
+        static void Display(string _firstName, string _lastName)
+        {
+            Console.WriteLine("MY FULL NAME");
+            Console.WriteLine("{0} {1}", _firstName, _lastName );
+        }
+
+
+    }
+}
+
