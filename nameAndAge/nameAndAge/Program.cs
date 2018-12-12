e23066ac6d7c26341c48f53db37778be7a23ac98
Hamish Wadsworth <hamishwaddy@gmail.com>
2018-12-12 14:10:46 +1300

    adding code

---
+++ nameAndAge/nameAndAge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace nameAndAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ", how are you?");
+            Console.WriteLine("How old are you? ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your gender? ");
+            string gender = Console.ReadLine();
+            Console.WriteLine("What is your street address? ");
+            string address = Console.ReadLine();
+            Console.Clear();
+            Display(name, age, gender, address);
+        }
+
+        static void Display(string _name, int _age, string _gender, string _address)
+
+        {
+        Console.WriteLine("USER DETAILS");
+        Console.WriteLine("Name: " + _name + "\nAge: " + _age + "\nGender: " + _gender + "\nAddress: " + _address);
+}
+    }
+}
---
+++ startingApp/startingApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace startingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 0;
+            do
+            {
+                Console.WriteLine("Type a 2-digit number");
+                num1 = int.Parse(Console.ReadLine());
+            } while (num1 <= 10 || num1 >= 99);
+
+
+            int num2 = 0;
+            do
+            {
+                Console.WriteLine("Next, type a 3-digit number");
+                num2 = int.Parse(Console.ReadLine());
+            } while (num2 <= 100 || num2 >= 999);
+
+            Console.WriteLine("FULL EQUATION");
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+        }
+
+    }
+}
