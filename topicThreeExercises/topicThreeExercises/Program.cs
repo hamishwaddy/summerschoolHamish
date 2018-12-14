fa3d3bc8e434222dbc08e9e0b71c2f658319f213
Hamish Wadsworth <hamishwaddy@gmail.com>
2018-12-14 20:43:32 +1300

    finished topic 3 but error on palindrome activity

--- topicThreeExercises/topicThreeExercises/Program.cs
+++ topicThreeExercises/topicThreeExercises/Program.cs
@@ -8,6 +8,9 @@ namespace topicThreeExercises
         {
 
             Console.WriteLine("Welcome to Topic 3: \nPART ONE\nConditional Statements and Program Flow");
+
+            /* ------------ Task 1 ------------ */
+
             Console.WriteLine("Please type a number and press the ENTER key.");
             int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Please type another number and press the ENTER key");
@@ -28,6 +31,7 @@ namespace topicThreeExercises
             Console.Write("Task 1 complete. Press ENTER to continue...");
             Console.ReadLine();
             Console.Clear();
+                    
             /* ------------ End of task 1 ------------ */
 
 
@@ -35,6 +39,7 @@ namespace topicThreeExercises
 
 
             /* ------------ Task 2 ------------ */
+
             Console.WriteLine("PART TWO");
             int myNum = 7;
 
@@ -48,8 +53,6 @@ namespace topicThreeExercises
                 else //guess was too low
                 Console.WriteLine("Too low, you should have tried with a higher number...");
 
-                
-            
 
             Console.WriteLine("Task two finished. Press ENTER to continue.");
             Console.ReadLine();
@@ -61,6 +64,7 @@ namespace topicThreeExercises
 
 
             /* ------------ Task 3 ------------ */
+
             Console.WriteLine("PART THREE");
             Console.WriteLine("Please enter your final mark for your course.");
             float mark = float.Parse(Console.ReadLine());
@@ -92,6 +96,7 @@ namespace topicThreeExercises
 
 
             /* ------------ Task 4 ------------ */
+
             Console.WriteLine("PART FOUR");
             Console.WriteLine("For my next trick, I will determine whether the number you enter is odd or even.");
             Console.Write("Please enter a number and press enter: ");
@@ -112,6 +117,7 @@ namespace topicThreeExercises
 
 
             /* ------------ Task 5 ------------ */
+
             Console.WriteLine("PART FIVE");
             Console.Write("Enter a 4-digit PIN number and press ENTER: ");
             int pin1 = int.Parse(Console.ReadLine());
@@ -134,6 +140,37 @@ namespace topicThreeExercises
             Console.ReadLine();
             Console.Clear();
             /* ------------ End of task 5 ------------ */
+
+
+
+
+            /* ------------ Task 6 ------------ */
+            Console.WriteLine("PART SIX: Palindrome");
+            Console.WriteLine("Think of a 3-letter palindrome.\nIt must begin and end with the same letter and have a vowel as the middle letter.");
+            Console.Write("Type the first letter of your palindrome: ");
+            string letter1 = Console.ReadLine();
+            Console.Write("Type the second letter (a vowel) of your palindrome: ");
+            string letter2 = Console.ReadLine();
+            Console.Write("Type the third letter of your palindrome: ");
+            string letter3 = Console.ReadLine();
+            Console.WriteLine("Here is your word: {0}{1}{2}", letter1,letter2,letter3);
+
+
+            if (letter1 == letter3)
+            {
+                if (letter2 == "a" || letter2 = "e" || letter2 = "i" || letter2 = "o" || letter2 = "u")
+                Console.WriteLine("Well done, your word '{0}{1}{2}' is a palindrome.", letter1, letter2, letter3);
+                else 
+                Console.WriteLine("This is not an actual word.");
+            }
+            else
+                Console.WriteLine("{0}{1}{2} is NOT a palindrome.", letter1, letter2, letter3);
+
+            Console.WriteLine("This is the end of Task 6");
+            Console.ReadLine();
+            Console.Clear();
+
+            /* ------------ End of task 6 ------------ */
 
 
         }
