e6d4015fefd99a01fa811874c91d2691a2df57ed
Hamish <hamishwaddy@gmail.com>
2018-12-24 18:05:34 +1300

    finished task 1

--- topicFourLoops/topicFourLoops/Program.cs
+++ topicFourLoops/topicFourLoops/Program.cs
@@ -9,17 +9,37 @@ namespace topicFourLoops
 
             Console.WriteLine("TOPIC FOUR - Loops\nExercise One:");
 
+            Console.WriteLine("N\t10*N\t100*N\t100*N");
+            Console.WriteLine("-\t----\t-----\t------");
 
 
-            Console.WriteLine(">>> while loop <<<");
-            int[] arr = new int[4];
+            Console.WriteLine("\n>>> for loop <<<");
+            
+            for (int n = 3; n >=3 && n<=7; n++)
+            {
+                Console.WriteLine(n + "\t" + n * 10 + "\t" + n * 100 + "\t" + n * 1000);
+            }
+
+
+            Console.WriteLine("\n>>> while loop <<<");
            
            int i = 3;
             while (i >= 3 && i <= 7) {
                 Console.WriteLine(i+"\t"+i*10+"\t"+i*100+"\t"+i*1000);
                 i++;
                 }
-            
+
+
+            Console.WriteLine("\n>>> do while loop <<<");
+
+            int x = 3;
+            do
+            {
+                Console.WriteLine(x + "\t" + x * 10 + "\t" + x * 100 + "\t" + x * 1000);
+                x++;
+            } while (x <= 7);
+
+
             Console.ReadKey();
         }
     }
